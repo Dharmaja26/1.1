12522fb8cfdbae333f51435ad0e5e8caa1544841
Dharmaja26 <165750322+Dharmaja26@users.noreply.github.com>
2024-04-14 00:46:54 +0530

    Add files via upload

---
+++ src/pages/loginPage.jsx
@@ -0,0 +1,16 @@
+import Forget from '../components/forget';
+import Login from '../components/login';
+import Movie from '../components/movie';
+import Reset from '../components/reset';
+
+function LoginPage() {
+  return (
+    <>
+   <Login />
+   <Forget/>
+   <Movie/>
+   </>
+  );
+}
+
+export default LoginPage;
---
+++ src/pages/moviePage.jsx
@@ -0,0 +1,11 @@
+import Movie from "../components/movie";
+
+
+function MoviePage  () {
+
+  return (
+    <Movie/>
+  );
+}
+
+export default MoviePage;
